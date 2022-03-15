cfbe00bef50c7afd055ec19ca52ec04f3ba8be2e
Rebecca Putnam <rebeccaputnam@gmail.com>
2022-03-15 11:11:36 +0100

    art script & background music

--- Assets/Script/Art.cs
+++ Assets/Script/Art.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class Art : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public AudioClip coinsound;
+    private AudioSource audioSource;
+
+    void Awake()
     {
-        
+        GetComponent<Collider2D>().isTrigger = true;
     }
 
-    // Update is called once per frame
-    void Update()
+    void OnTriggerEnter2D(Collider2D other)
     {
         
+        if (other.CompareTag("Player"))
+        {
+
+            Destroy(gameObject);
+            // audioSource.PlayOneShot(coinsound);
+
+
+
+
+        }
+
     }
+
+
 }
