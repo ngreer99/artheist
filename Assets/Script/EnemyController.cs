dae104b160b77e89fc7022e991efef915d61190e
ngreer99 <nora.ld.g@gmail.com>
2022-03-15 11:41:30 +0100

    security guard movement

--- Assets/Script/EnemyController.cs
+++ Assets/Script/EnemyController.cs
@@ -1,35 +1,52 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+//artheist 
 
-public class EnemyController : BaseController
+public class Boundary
 {
-    public float ledgeTestLeft;
-    public float ledgeTestRight;
+    public float xMin, xMax;
+}
+
+public class EnemyController : MonoBehaviour
+{
+    //public float ledgeTestLeft;
+    //public float ledgeTestRight;
 
     private int direction = 1;
-    private float walkTimer = 0;
+    //private float walkTimer = 0;
+
+    public Transform transformx;
+    private Vector3 xAxis;
+    private float secondsForOneLength = 1f;
+    public Boundary boundary;
+    
 
     void Update()
     {
-        UpdateGrounding();
-        UpdateDirection();
+        //UpdateGrounding();
+        //UpdateDirection();
 
-        Vector2 vel = rb2d.velocity;
-        vel.x = direction * speed;
-        rb2d.velocity = vel;
-        relativeVelocity = vel;
+        //Vector2 vel = rb2d.velocity;
+        //vel.x = direction * speed;
+        //rb2d.velocity = vel;
+        //relativeVelocity = vel;
+
+        transform.position = new Vector3(Mathf.PingPong(boundary.xMin, boundary.xMax), transform.position.y, transform.position.z);
+        
     }
 
-    int UpdateDirection()
-    {
 
+    /*int UpdateDirection()
+    {
+       
         walkTimer -= Time.deltaTime;
         if (walkTimer <= 3.0f)
         {
             direction = -1;
         }
         
+
         Vector3 ledgeRayStartLeft = transform.position + Vector3.up * groundRayLength + Vector3.left * ledgeTestLeft;
         Vector3 ledgeRayStartRight = transform.position + Vector3.up * groundRayLength + Vector3.right * ledgeTestRight;
 
@@ -39,7 +56,7 @@ public class EnemyController : BaseController
         RaycastHit2D hitLeft = Physics2D.Raycast(ledgeRayStartLeft, Vector2.down, groundRayLength * 2, groundLayers);
         RaycastHit2D hitRight = Physics2D.Raycast(ledgeRayStartRight, Vector2.down, groundRayLength * 2, groundLayers);
 
-        /*if (hitLeft.collider == null)
+        if (hitLeft.collider == null)
         {
             direction = 1;
         }
@@ -47,11 +64,17 @@ public class EnemyController : BaseController
         if (hitRight.collider == null)
         {
             direction = -1;
-        }*/
+        }
         return direction;
+        
+        
+    }*/
+    void OnTriggerEnter(Collider Player)
+    {
+        Destroy(Player.gameObject);
     }
 
-    protected override void Hurt(Vector3 impactDirection)
+    /*protected override void Hurt(Vector3 impactDirection)
     {
         if (Mathf.Abs(impactDirection.x) > Mathf.Abs(impactDirection.y))
         {
@@ -66,4 +89,5 @@ public class EnemyController : BaseController
         }
 
     }
+    */
 }
