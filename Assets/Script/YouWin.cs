90265105fb755d57bf661da97490cfc311f1beaa
Jane Urciuoli <jurciuol@bowdoin.edu>
2022-03-19 13:40:07 +0100

    Not Sure

--- Assets/Script/YouWin.cs
+++ Assets/Script/YouWin.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class YouWin : MonoBehaviour
 {
+<<<<<<< Updated upstream
     // Start is called before the first frame update
     void Start()
     {
         
+=======
+    public float speed;
+    private Rigidbody2D myRb2D;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRb2D = GetComponent<Rigidbody2D>();
+>>>>>>> Stashed changes
     }
 
     // Update is called once per frame
     void Update()
     {
+<<<<<<< Updated upstream
         transform.localPosition *= (1, 1, 1);
+=======
+        Vector2 vel = myRb2D.velocity;
+        vel.x = speed;
+        myRb2D.velocity = vel;
+>>>>>>> Stashed changes
     }
 }
